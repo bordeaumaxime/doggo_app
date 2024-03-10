a8510892ab0accc26523421d814d4b837c8db53c
bordeaumaxime <bordeau.maxime@gmail.com>
2024-03-10 10:00:58 +0000

    Add HTTP cache

--- Doggo/app/src/main/java/com/doggo/di/NetworkModule.kt
+++ Doggo/app/src/main/java/com/doggo/di/NetworkModule.kt
@@ -1,10 +1,14 @@
 package com.doggo.di
 
+import android.content.Context
 import com.doggo.data.network.DogCeoApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,9 +19,21 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideOkHttpClient(@ApplicationContext appContext: Context): OkHttpClient {
+        val cacheMaxSize = 5L * 1000L * 1000L // 5MB
+        val cache = Cache(appContext.cacheDir, cacheMaxSize)
+        return OkHttpClient
+            .Builder()
+            .cache(cache)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(DogCeoApiService.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
